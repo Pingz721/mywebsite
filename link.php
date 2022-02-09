4a04d6a72656646b2d3ff493d15dd98dff13b512
Pingz721 <97584415+Pingz721@users.noreply.github.com>
2022-02-08 23:17:51 -0800

    Update link.php

--- link.php
+++ link.php
@@ -1,8 +1,8 @@
 <html>
   <head>
-    <title>
+    <title>การเชื่อมโยงเว็บไซต์</title>
   </head>
   <body
-        <a href="https://lomsak.ac.th"><h1>วาร์ปของฉัน</h1></a>
+      <a href="https://lomsak.ac.th"><h1>วาร์ปของฉัน</h1></a>
   </body>
 </html>
