17dbde608e01c0ede5a8e267f1d48e39766d6c12
Pingz721 <97584415+Pingz721@users.noreply.github.com>
2022-02-08 23:54:41 -0800

    Update mypage2.php

--- mypage2.php
+++ mypage2.php
@@ -3,6 +3,6 @@
     <title>การเชื่อมโยงเว็บไซต์</title>
   </head>
   <body>
-    <a href="mypage1.php"><h1>วาร์ปของฉัน</h1></a>
+    <a href="mypage1.php"><h1>Back to page 1</h1></a>
   </body>
 </html>
