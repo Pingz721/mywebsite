546e1f0c1e605e5f7fedeafa513a552bae475325
Pingz721 <97584415+Pingz721@users.noreply.github.com>
2022-02-08 23:49:50 -0800

    Create mypage1.php

---
+++ mypage1.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>การบ้านการเชื่อมโยงเว็บไซต์</title>
+  </head>
+  <body>
+    <a href="https://facebook.com"><h1>Facebook</h1></a>
+    <a href="mypage2.php"><h1>Facebook</h1></a>
+  </body>
+</html>
