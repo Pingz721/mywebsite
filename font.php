3e63c433f215bbd95f9612d8d2838811be6f32c4
Pingz721 <97584415+Pingz721@users.noreply.github.com>
2022-02-22 22:31:05 -0800

    Update font.php

--- font.php
+++ font.php
@@ -7,17 +7,19 @@
         src: url("FCParagraph[Non-commercial]-Bold.ttf");
       }
       @font-face{
-        font-family: "MyFont1";
-        src: url("FCParagraph[Non-commercial]-Bold.ttf");
+        font-family: "MyFont2";
+        src: url("TALullabyNM-LightItalic.ttf");
       }
       #font1{
         font-family: "MyFont1";
       }
-      #font1{
-        font-family: "MyFont1";
+      #font2{
+        font-family: "MyFont2";
+      }
     </style>
   </head>
   <body>
-    <h1>ฟอนต์ของฉัน</h1>
+    <h1 id="font1">นางสาวเปี่ยมปีติ ปอแก้ว เลขที่ 18 ห้อง4/12</h1>
+    <h1 id="font1">นางสาวเปี่ยมปีติ ปอแก้ว เลขที่ 18 ห้อง4/12</h1>
   </body>
 </html>
