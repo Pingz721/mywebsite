ca8022d5fabbdc438df2dcdf0611998c17fa9911
Pingz721 <97584415+Pingz721@users.noreply.github.com>
2022-03-02 00:35:11 -0800

    Update table.php

--- table.php
+++ table.php
@@ -13,21 +13,21 @@
       
      <tr>
        <td>1</td>
-       <td style="color:#B74907;>Hakken Ryou</td>
+       <td style="color:#B74907;">Hakken Ryou</td>
        <td><img src="https://pbs.twimg.com/media/DZnm95lVoAEYNDC?format=jpg&name=large" width="100px"></td>
        <td><a href="https://mobile.twitter.com/hakkenryou">Twitter<a/></td>
      </tr>
       
       <tr>
        <td>2</td>
-       <td style="color:#6E9733;>Xiao Zhan</td>
+       <td style="color:#6E9733;">Xiao Zhan</td>
        <td><img src="http://puui.qpic.cn/qqvideo_ori/0/c00332gzq50_496_280/0" width="100px"></td>
        <td><a href=" https://weibo.com/xiaozhan1">Weibo<a/></td>
      </tr>
       
      <tr>
        <td>3</td>
-       <td style="color:#BC0000;>Vox Akuma</td>
+       <td style="color:#BC0000;">Vox Akuma</td>
        <td><img src="https://pbs.twimg.com/media/FHLMO-aXwAU_rZS.jpg" width="100px"></td>
        <td><a href="https://twitter.com/Vox_Akuma">Twitter<a/></td>
      </tr>
@@ -41,7 +41,7 @@
       
      <tr>
        <td>5</td>
-       <td style="color:#EA317F;>泠鸢(Yousa)</td>
+       <td style="color:#EA317F;">泠鸢(Yousa)</td>
        <td><img src="https://i.ytimg.com/vi/BKblrXHumDk/maxresdefault.jpg" width="100px"></td>
        <td><a href="https://weibo.com/lingyuanyousa?refer_flag=1005050010_">Weibo<a/></td>
      </tr>
