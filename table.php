9c484ce26668b876039b38508b9b8beee2e18911
Pingz721 <97584415+Pingz721@users.noreply.github.com>
2022-03-01 22:40:04 -0800

    Update table.php

--- table.php
+++ table.php
@@ -5,15 +5,22 @@
   <body>
     <table border="1">
      <tr>
-       <th>คอลัมน์ที่1</th>
-       <th>คอลัมน์ที่2</th>
-       <td><b>คอลัมน์ที่3</b></td>
+       <th>No.</th>
+       <th>Name</th>
+       <th>Picture</th>
+       <th>Social Media</th>
      </tr>
      <tr>
-       <td>คอลัมน์ที่1</td>
-       <td>คอลัมน์ที่2</td>
-       <td><img src="https://pbs.twimg.com/media/DZnm95lVoAEYNDC?format=jpg&name=large" width="45px"></td>
+       <td>1</td>
+       <td>Hakken Ryou</td>
+       <td><img src="https://pbs.twimg.com/media/DZnm95lVoAEYNDC?format=jpg&name=large" width="100px"></td>
        <td><a href="https://mobile.twitter.com/hakkenryou">Twitter<a/></td>
+     </tr>
+      <tr>
+       <td>   </td>
+       <td>   </td>
+       <td><img src="   " width="100px"></td>
+       <td><a href="   ">Twitter<a/></td>
      </tr>
     </table>
   </body>
