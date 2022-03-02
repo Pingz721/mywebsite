99f6fed359166b4bdf5aef574644c239bed40ff9
Pingz721 <97584415+Pingz721@users.noreply.github.com>
2022-03-01 22:35:08 -0800

    Update table.php

--- table.php
+++ table.php
@@ -12,7 +12,7 @@
      <tr>
        <td>คอลัมน์ที่1</td>
        <td>คอลัมน์ที่2</td>
-       <td><img src="" width="45px"></td>
+       <td><img src="https://pbs.twimg.com/media/DZnm95lVoAEYNDC?format=jpg&name=large" width="45px"></td>
        <td><a href="https://mobile.twitter.com/hakkenryou">Twitter<a/></td>
      </tr>
     </table>
