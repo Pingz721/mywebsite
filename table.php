66b0838bc387363968f167ef96e45e67a31fb9bb
Pingz721 <97584415+Pingz721@users.noreply.github.com>
2022-03-01 22:41:44 -0800

    Update table.php

--- table.php
+++ table.php
@@ -19,7 +19,7 @@
       <tr>
        <td>   </td>
        <td>   </td>
-       <td><img src="   " width="100px"></td>
+       <td><img src="https://64.media.tumblr.com/6b21931985e1ed194d59962027a78366/b05109c57ee4307a-ce/s540x810/368cf79d85f11437a95a9a5aabc0f163adf1c1bb.jpg" width="100px"></td>
        <td><a href="   ">Twitter<a/></td>
      </tr>
     </table>
